49c1de862ac21ee07e12a887a5991bad27191c56
danielbengtsen <cph-db145@cphbusiness.dk>
2021-04-29 16:30:18 +0200

    animations added for player to old input system

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,51 +5,70 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-   private PlayerControls m_playerControls;
-   private Rigidbody player;
-   private float runSpeed = 5f;
-   private float jumpHeight = 1.5f;
-   private bool isGrounded = true;
-    
-    private void Awake() {
+    private PlayerControls m_playerControls;
+    private Rigidbody player;
+    private float runSpeed = 5f;
+    private float jumpHeight = 1.5f;
+    private bool isGrounded = true;
+    private Animator animator;
+
+    private void Awake()
+    {
         m_playerControls = new PlayerControls();
         player = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         player.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
     }
-    
+
 
     void Update()
     {
-        if(Keyboard.current != null && Keyboard.current.dKey.isPressed)
+        bool anyKeyPressed = (Keyboard.current != null);
+        bool dKeyPressed = (Keyboard.current.dKey.isPressed);
+        bool aKeyPressed = (Keyboard.current.aKey.isPressed);
+        bool spacePressed = (Keyboard.current.spaceKey.wasPressedThisFrame);
+
+        if (anyKeyPressed && dKeyPressed)
         {
+            animator.SetBool("isRunning", true);
             player.transform.position += Vector3.right * runSpeed * Time.deltaTime;
         }
-        if(Keyboard.current != null && Keyboard.current.aKey.isPressed)
+
+
+        if (anyKeyPressed && aKeyPressed)
         {
+            animator.SetBool("isRunning", true);
             player.transform.position += Vector3.right * -runSpeed * Time.deltaTime;
         }
-        if(Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+
+        if (anyKeyPressed && !aKeyPressed && !dKeyPressed)
         {
+            animator.SetBool("isRunning", false);
+        }
+
+
+        if (anyKeyPressed && spacePressed && isGrounded)
+        {
+            animator.SetTrigger("isJumping");
             player.transform.position += Vector3.up * jumpHeight;
-            
-        }  
+        }
     }
 
     void OnCollisionEnter(Collision other)
- {
-     if (other.gameObject.tag == "Ground")
-     {
-         isGrounded = true;
-     }
- }
- 
- void OnCollisionExit(Collision other)
- {
-     if (other.gameObject.tag == "Ground")
-     {
-         isGrounded = false;
-     }
- }
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
 
     void OnEnable()
     {
