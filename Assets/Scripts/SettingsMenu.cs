32d4b4d2fbe57f10aef8de2ce1c3d28e0b6ce5f7
Bloom9900 <jannichhoejmose@gmail.com>
2021-06-13 15:53:46 +0200

    Huske at tjekke merge konflikter næste gang drenge :)

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class SettingsMenu : MonoBehaviour
 {
+
+    /* public Audio sound; */ //Example
     public void SetVolume(float volume) {
         /* To adjust volume when it's made, make an instance of the class in the audio script
         and adjust the variable in that way. See link for guide:
         https://forum.unity.com/threads/how-to-change-a-variable-from-another-script.701915/ From Optrix */
         /* Debug.Log(volume); */
+        //sound.volume = volume; Example
+
     }
 
     
